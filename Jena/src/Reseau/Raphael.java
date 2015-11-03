c0fdb417d643fa44e6bf2a241a48a88ea52bc102
Reiji7 <adrien_claudel@icloud.com>
2015-11-03 08:59:09 +0100

    Ajout 3 client

---
+++ Jena/src/Reseau/Raphael.java
@@ -0,0 +1,28 @@
+package Reseau;
+
+import java.net.*;
+import java.util.Scanner;
+
+public class Raphael {
+    
+    public static void main(String[] zero) {
+        
+        int port = 2000;
+        
+        try{
+            
+            Client Raphael = new Client(InetAddress.getLocalHost(), port, "Raphael");
+            Raphael.start();
+
+        }catch (UnknownHostException e) {e.printStackTrace();}
+    }
+}
+
+
+/*
+ try{
+ //pouette
+ }
+ catch (UnknownHostException e) {e.printStackTrace();}
+ catch (IOException e) {e.printStackTrace();}
+ */
