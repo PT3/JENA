66914ebd7e33340ad4647255ef41e9e85827e895
Reiji7 <adrien_claudel@icloud.com>
2015-11-19 14:55:57 +0100

    Ajout de ChatServeur

---
+++ Jena/src/Reseau/ChatServeur.java
@@ -0,0 +1,51 @@
+package Reseau;
+
+import java.io.*;
+import java.net.*;
+
+public class ChatServeur implements Runnable {
+
+    private String login;		// Login du client
+    private Socket socket;		// lien avec le socket Client
+    private PrintWriter out;	// Envoyeur
+    private BufferedReader in;	// Receveur
+    private Thread tRecep, tEmiss;	// Thread de reception et d'émission des message
+    
+    
+    /**
+     * Constructeur du chat d'un client
+     * @param s
+     * @param login
+     */
+    public ChatServeur(Socket s, String login){
+        this.socket = socket;
+        this.login = login;
+    }
+
+    
+    /**
+     * Lancement d'une procédure de chat
+     */
+    public void run(){
+        
+        try {
+            
+            // Initialisation du receveur
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            // Initialisation de l'envoyeur
+            out = new PrintWriter(socket.getOutputStream());
+            
+            // Thread de reception des messages 
+            Thread tRecep = new Thread(new Reception(in, out, login));
+            tRecep.start();
+            
+            // Thread d'envoie des messages depuis le serveur
+            Thread tEmiss = new Thread(new Emission(out));
+            tEmiss.start();
+            
+        }catch (IOException e)
+        {System.err.println(login + " a été déconnecté ");}
+        
+    }
+}
