51288d16088c8e418afa3a687e530f14fc45ef47
popotheone <nicolas.chole.nc@gmail.com>
2015-11-30 17:28:57 +0100

    Ajout inscription

---
+++ Jena/src/inscription/Main.java
@@ -0,0 +1,12 @@
+package iGraphique;
+
+import java.awt.Dimension;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Login l = new Login(250,320);
+		/*Inscription i = new Inscription(250,320);*/
+	}
+}
